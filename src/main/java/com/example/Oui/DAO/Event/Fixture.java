4678c30834fdc3ee470e96327d809e2f3cc68a2a
CharlesDesrayaud1 <charles.desrayaud@gmail.com>
2023-09-16 20:20:27 +0200

    fixing fixture **

--- src/main/java/com/example/Oui/DAO/Event/Fixture.java
+++ src/main/java/com/example/Oui/DAO/Event/Fixture.java
@@ -1,5 +1,6 @@
 package com.example.Oui.DAO.Event;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 
@@ -8,7 +9,6 @@ import java.util.Date;
 
 @Entity
 @Table(name = "fixture")
-@AllArgsConstructor
 public class Fixture {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,5 +20,24 @@ public class Fixture {
     private String stadium;
     private String referee;
     private Date date;
+
+    @JsonCreator
+    public Fixture(@JsonProperty("id") int id,
+                @JsonProperty("team_home") String team_home,
+                @JsonProperty("team_away") String team_away,
+                @JsonProperty("match_type") String match_type,
+                @JsonProperty("stadium") String stadium,
+                @JsonProperty("referee") String referee,
+                @JsonProperty("date") Date date
+    ){
+        this.id = id;
+        this.team_home = team_home;
+        this.team_away = team_away;
+        this.match_type = match_type;
+        this.stadium = stadium;
+        this.referee = referee;
+        this.date = date;
+
+    }
 
 }
